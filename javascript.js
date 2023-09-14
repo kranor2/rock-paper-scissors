bd897bfd5c44b4f8cb4af5d5b0f4c703fab62de7
Rhett North <retsrawk@gmail.com>
2023-09-13 18:51:44 -0600

    Add convert function for computerSelection
    In this commit, I have added an if/elseif/else function to convert the
    integer value to the string value (rock, paper, or scissors) in order to
    make the computer compete with the user properly. I could have done this
    instead by converting the user's selection to an integer and adjust the
    process accordingly to compare the two integers, rather than two
    strings, but I chose this route as it feels more efficient to only enact
    one conversion rather than three (as I would eventually convert both
    user and computer values to strings). I adjusted the comments to
    properly document the process and inform the direction of the process
    based on the comparison route I chose.

--- javascript.js
+++ javascript.js
@@ -8,13 +8,29 @@
 // Process
 let playerSelection = prompt('Choose your weapon:', 'type "rock", "paper", or "scissors" here'); // prompt: ask the user 'Choose your weapon:' with choices in default text display
     console.log(playerSelection); // store prompt answer in variable 'playerSelection'
-function getRandomInt(min, max) {
+function getComputerChoice(min, max) {
     return Math.floor(Math.random() * (3 - 1 + 1) + 1);
 }
-var computerSelection = getRandomInt(1, 3); // (function getComputerChoice) browser/computer generates random selection between 'rock', 'paper', or 'scissors'
-    console.log(computerSelection) // store answer in variable 'computerSelection'
+var computerChoice = getComputerChoice(1, 3); // (function getComputerChoice) browser/computer generates random integer between 1 and 3; store in variable 'computerChoice'
+console.log(computerChoice)
 
-// convert playerSelection to integer, wherein rock = 1, paper = 2, scissors = 3
+function getComputerSelection() {
+    if (computerChoice === 1) {
+        return "rock"
+    }
+    else if (computerChoice === 2) {
+        return "paper"
+    }
+    else if (computerChoice === 3) {
+        return "scissors"
+    }
+    else {
+        return "forfeit"
+    }
+}
+var computerSelection = getComputerSelection("rock" | "paper" | "scissors" | "forfeit") // convert playerSelection to integer, wherein rock = 1, paper = 2, scissors = 3
+console.log(computerSelection)
+
 // (function playRound) compare playerSelection vs computerSelection based on rules (use if/else if/else)
 // if computerSelection beats (>) playerSelection => return string 'You Lose! [item] beats [item]'
 // if computerSelection loses to (<) playerSelection => return string 'You Win! [item] beats [item]'
