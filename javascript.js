789764a35e5c382384f3e38c1bf29d6f330005d9
Rhett North <retsrawk@gmail.com>
2023-09-13 19:41:14 -0600

    Add single-round comparison function
    In this commit, I have added a more complex if/elseif/else function to
    compare computerSelection and playerSelection, using the results to
    declare a winner. I also revised the structure between comments and
    code to make the document more readable.

--- javascript.js
+++ javascript.js
@@ -6,12 +6,19 @@
 // rock beats (>) scissors
 
 // Process
-let playerSelection = prompt('Choose your weapon:', 'type "rock", "paper", or "scissors" here'); // prompt: ask the user 'Choose your weapon:' with choices in default text display
-    console.log(playerSelection); // store prompt answer in variable 'playerSelection'
-function getComputerChoice(min, max) {
+// prompt: ask the user 'Choose your weapon:' with choices in default text display
+// store prompt answer in variable 'playerSelection'
+
+let playerSelection = prompt('Choose your weapon:', 'type "rock", "paper", or "scissors" here'); 
+    console.log(playerSelection); 
+
+// (function getComputerChoice) browser/computer generates random integer between 1 and 3
+// store answer in variable 'computerChoice'
+
+    function getComputerChoice(min, max) {
     return Math.floor(Math.random() * (3 - 1 + 1) + 1);
 }
-var computerChoice = getComputerChoice(1, 3); // (function getComputerChoice) browser/computer generates random integer between 1 and 3; store in variable 'computerChoice'
+var computerChoice = getComputerChoice(1, 3); 
 console.log(computerChoice)
 
 function getComputerSelection() {
@@ -28,6 +35,7 @@ function getComputerSelection() {
         return "forfeit"
     }
 }
+
 var computerSelection = getComputerSelection("rock" | "paper" | "scissors" | "forfeit") // convert playerSelection to integer, wherein rock = 1, paper = 2, scissors = 3
 console.log(computerSelection)
 
@@ -35,6 +43,42 @@ console.log(computerSelection)
 // if computerSelection beats (>) playerSelection => return string 'You Lose! [item] beats [item]'
 // if computerSelection loses to (<) playerSelection => return string 'You Win! [item] beats [item]'
 // if computerSelection ties (===) playerSelection => return string 'It's a Tie! Try again.'
+
+function playRound() {
+    if (computerSelection === "rock" && playerSelection === "rock") {
+        return "It's a Tie! Try again."
+    }
+    else if (computerSelection === "rock" && playerSelection === "scissors") {
+        return "You Lose! Rock beats Scissors"
+    }
+    else if (computerSelection === "scissors" && playerSelection === "rock") {
+        return "You Win! Rock beats Scissors"
+    }
+    else if (computerSelection === "paper" && playerSelection === "paper") {
+        return "It's a Tie! Try again."
+    }
+    else if (computerSelection === "paper" && playerSelection === "rock") {
+        return "You Lose! Paper beats Rock"
+    }
+    else if (computerSelection === "rock" && playerSelection === "paper") {
+        return "You Win! Paper beats Rock"
+    }
+    else if (computerSelection === "scissors" && playerSelection === "scissors") {
+        return "It's a Tie! Try again."
+    }
+    else if (computerSelection === "scissors" && playerSelection === "paper") {
+        return "You Lose! Scissors beats Paper"
+    }
+    else if (computerSelection === "paper" && playerSelection === "scissors") {
+        return "You Win! Scissors beats Paper"
+    }
+    else {
+        return "Error. Please refresh the page."
+    }
+}
+
+var roundOutcome = playRound("It's a Tie! Try again." | "You Lose! Rock beats Scissors" | "You Win! Rock beats Scissors" | "You Lose! Paper beats Rock" | "You Win! Paper beats Rock" | "You Lose! Scissors beats Paper" | "You Win! Scissors beats Paper" | "Error. Please refresh the page.")
+console.log(roundOutcome)
 
 // Game (function game())
 // Repeat the above 'Process' 5 times
